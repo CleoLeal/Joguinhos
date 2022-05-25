5735c49f60cc8bccefa6ad0398c65aee4b40b97a
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-05-25 16:48:26 -0300

    Atualização
    Após correções no if e no while do programa. Ele funciona corretamente

--- sorte.py
+++ sorte.py
@@ -1,33 +1,33 @@
 import random
+from turtle import clear
 jogador = 0
 computador = 0
-escolha = 0
+escolha = -1
 
-while escolha !=0:
-    print("Escolha: \n1–Ímpar \n2–Par \n0–Sair")
-    escolha=input()
+while escolha != 0:
+    escolha = int(input("Escolha: \n1–Ímpar \n2–Par \n0–Sair"))
     if escolha == 0:
         exit()
-    gerado = random.randint(0,100)
-    if escolha == 1 & gerado%2==0:
-        computador+=1
-        print("Você escolheu Impar, e o número",gerado,"é Par.\nVocê errou!")
+    gerado = random.randint(0, 100)
+    if escolha == 1 and (gerado % 2) == 0:
+        computador += 1
+        print("Você escolheu Ímpar, e o número", gerado, "é Par. Você errou!")
         print("------Placar------")
-        print("Jogador -",jogador,"\nComputador -",computador)
-    elif escolha == 1 & gerado%2!=0:
-        jogador+=1
-        print("Você escolheu Impar, e o número",gerado,"é Ímpar.\nVocê acertou!")
+        print("Jogador -", jogador, "\nComputador -", computador)
+    elif escolha == 1 and (gerado % 2) != 0:
+        jogador += 1
+        print("Você escolheu Ímpar, e o número", gerado, "é Ímpar. Você Acertou!")
         print("------Placar------")
-        print("Jogador -",jogador,"\nComputador -",computador)
-    elif escolha == 2 & gerado%2==0:
-        jogador+=1
-        print("Você escolheu Par, e o número",gerado,"é Par.\nVocê acertou!")
+        print("Jogador -", jogador, "\nComputador -", computador)
+    elif escolha == 2 and (gerado % 2) == 0:
+        jogador += 1
+        print("Você escolheu Par, e o número", gerado, "é Par. Você acertou!")
         print("------Placar------")
-        print("Jogador -",jogador,"\nComputador -",computador)
-    elif escolha == 2 & gerado%2!=0:
-        computador+=1
-        print("Você escolheu Par, e o número",gerado,"é Ímpar.\nVocê errou!")
+        print("Jogador -", jogador, "\nComputador -", computador)
+    elif escolha == 2 and (gerado % 2) != 0:
+        computador += 1
+        print("Você escolheu Par, e o número", gerado, "é Ímpar. Você errou!")
         print("------Placar------")
-        print("Jogador -",jogador,"\nComputador -",computador)
-    else:
-        print("Você informou algo diferente de 0, 1 e 2!!")
+        print("Jogador -", jogador, "\nComputador -", computador)
+
+    escolha = input()
